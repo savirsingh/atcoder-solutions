db5558f2627cb8c1c5338adcd60ca9486e7facfc
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-03-13 21:30:28 -0400

    Create A.cpp

---
+++ abc293/A.cpp
@@ -0,0 +1,18 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+//#define int long long
+//const int MAX = 1e6+2;
+
+string s;
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> s;
+    for (int i = 1; i <= s.size()/2; i++) {
+        swap(s[2*i-2], s[2*i-1]);
+    }
+    cout << s << endl;
+}
