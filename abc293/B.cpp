a23b5076916b5b0e75b9e831484b4f7ede72dcd6
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-03-13 21:45:28 -0400

    Create B.cpp

---
+++ abc293/B.cpp
@@ -0,0 +1,33 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+//#define int long long
+const int MAX = 2e5+2;
+
+int n;
+int called[MAX], ans = 0;
+vector<int> res;
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n;
+    memset(called, false, sizeof called);
+    for (int i = 1, a; i <= n; i++) {
+        cin >> a;
+        if (called[i]) continue;
+        called[a] = true;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (!called[i]) {
+            ans++; res.push_back(i);
+        }
+    }
+    cout << ans << endl;
+    for (int i = 0; i < ans; i++) {
+        cout << res[i];
+        if (i != ans-1) cout << " ";
+    }
+    cout << endl;
+}
