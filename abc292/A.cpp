cbaf5b12a321a5e4812d6f22c3c31ccd6a47e3a5
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-03-16 18:55:40 -0400

    Create A.cpp

---
+++ abc292/A.cpp
@@ -0,0 +1,14 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+
+int main() {
+    cin >> s;
+    for (int i = 0; i < s.size(); i++) {
+        s[i] -= 32;
+    }
+    cout << s << endl;
+}
