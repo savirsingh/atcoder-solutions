0be33cc6407c19cf5b84e9fa1795d7d70189f5c6
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-03-17 13:21:20 -0400

    Create B.cpp

---
+++ abc291/B.cpp
@@ -0,0 +1,24 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pii pair<int, int>
+
+int n;
+
+int32_t main() {
+    cin >> n;
+    vector<int> nums;
+    for (int i = 1, x; i <= 5*n; i++) {
+        cin >> x;
+        nums.push_back(x);
+    }
+    sort(nums.begin(), nums.end());
+    double sum = 0;
+    for (int i = n; i < 4*n; i++) {
+        sum += nums[i];
+    }
+    sum /= 3*n;
+    cout << fixed << setprecision(15) << sum << endl;
+}
