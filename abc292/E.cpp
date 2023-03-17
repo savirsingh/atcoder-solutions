2995bf5097d13b9c81a5c15f65f5ad181a7de6d0
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-03-17 11:43:16 -0400

    Create E.cpp

---
+++ abc292/E.cpp
@@ -0,0 +1,43 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pii pair<int, int>
+ 
+const int MM = 2001;
+ 
+int n, m;
+vector<int> adj[MM];
+bool visited[MM];
+int reachable = 0;
+ 
+void bfs(int node) {
+    for (int i = 1; i <= n; i++) {
+        visited[i] = false;
+    }
+    deque<int> deq;
+    deq.push_back(node);
+    visited[node] = true;
+    while (!deq.empty()) {
+        int cur = deq.front();
+        deq.pop_front();
+        for (int nxt : adj[cur]) {
+            if (visited[nxt]) continue;
+            reachable++;
+            visited[nxt] = true;
+            deq.push_back(nxt);
+        }
+    }
+}
+ 
+int32_t main() {
+    cin >> n >> m;
+    for (int i = 1, u, v; i <= m; i++) {
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+    int ans = 0;
+    for (int i = 1; i <= n; i++) bfs(i);
+    cout << reachable - m << endl;
+}
