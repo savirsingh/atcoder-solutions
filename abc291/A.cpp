171410bc422462ea38bce646b7b5e3155229f053
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-03-17 13:08:03 -0400

    Create A.cpp

---
+++ abc291/A.cpp
@@ -0,0 +1,19 @@
+// code by savir singh
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pii pair<int, int>
+
+string s;
+
+int32_t main() {
+    cin >> s;
+    for (int i = 0; i < s.size(); i++) {
+        char j = s[i];
+        if (j < 97) {
+            cout << i+1 << endl;
+            break;
+        }
+    }
+}
