cd2419de65cb792b6db16f291c698c38adbf2ad9
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-04-09 10:45:48 -0400

    Create D.cpp

---
+++ abc297/D.cpp
@@ -0,0 +1,83 @@
+// code by savir singh
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define __ ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl '\n'
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vpii vector<pii>
+#define vvpii vector<vpii>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(a) (int) (a).size()
+#define rsz resize
+#define pii pair<int, int>
+#define eb emplace_back
+#define f(i,x,n) for(int i=x;i<n;i++)
+#define fe(i,x,n) for(int i=x;i<=n;i++)
+#define fr(i,x,n) for(int i=x;i>n;i--)
+#define fre(i,x,n) for(int i=x;i>=n;i--)
+#define mod 1000000007
+#define mod2 998244353
+#define INF 1e18
+#define ld long double
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define w(x) int x; cin>>x; while(x--)
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+const int MM = 101;
+int a, b;
+ 
+// fast i/o
+void read(int &number)
+{
+    bool negative = false;
+    register int c;
+ 
+    number = 0;
+    c = getchar();
+    if (c=='-')
+    {
+        negative = true;
+ 
+        c = getchar();
+    }
+    for (; (c>47 && c<58); c=getchar())
+        number = number *10 + c - 48;
+    if (negative)
+        number *= -1;
+}
+ 
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+ 
+int solve(int a, int b) {
+    int cnt = 0;
+    while (a != b) {
+        if (a > b) a -= b;
+        else b -= a;
+        cnt++;
+    }
+    return cnt;
+}
+ 
+int32_t main() {
+    __ read(a); read(b);
+    int z = gcd(a, b);
+    int sofar = (max(a, b) - z) / min(a, b);
+    //cout << sofar << endl;
+    if (a < b) swap(a, b);
+    a -= sofar * min(a, b);
+    cout << solve(a, b) + sofar << endl;
+}
